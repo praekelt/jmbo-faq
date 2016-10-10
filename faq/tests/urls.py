df20e1bb208f094f4682d765d25a82d663255db0
ruands <ruan.desousa@praekeltconsulting.com>
2016-10-10 14:53:21 +0200

    Added faq urls to test urls

--- faq/tests/urls.py
+++ faq/tests/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r"^admin/", include(admin.site.urls)),
-    url(r"^jmbo/", include("jmbo.urls"))
+    url(r"^jmbo/", include("jmbo.urls")),
+    url(r"^faq/", include("faq.urls"))
 ]
