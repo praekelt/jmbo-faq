e358bd295a3b96666aa1d47c6aa93852fe051287
Phala Ramonyai <phalamag@gmail.com>
2016-12-23 12:17:51 +0200

    Add detail template

--- faq/urls.py
+++ faq/urls.py
@@ -5,12 +5,16 @@ from jmbo.views import ObjectDetail
 urlpatterns = [
     url(
         r"^(?P<category_slug>[\w-]+)/(?P<slug>[\w-]+)/$",
-        ObjectDetail.as_view(),
+        ObjectDetail.as_view(
+            template_name="faq/faq_detail.html"
+        ),
         name="faq-categorized-detail"
     ),
     url(
         r"^(?P<slug>[\w-]+)/$",
-        ObjectDetail.as_view(),
+        ObjectDetail.as_view(
+            template_name="faq/faq_detail.html"
+        ),
         name="faq-detail"
     ),
 ]
